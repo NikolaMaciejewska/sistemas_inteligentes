b81255a63ab3596424517f59b535a2f00c8a9fc0
NikolaMaciejewska <247725@edu.p.lodz.pl>
2025-05-10 16:43:35 +0200

    lanzador de agentes

--- src/main/java/org/example/agents/MainContainerLauncher.java
+++ src/main/java/org/example/agents/MainContainerLauncher.java
@@ -1,4 +1,50 @@
 package org.example.agents;
 
-public class MainContainerLauncher extends javax.swing.JFrame {
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
+public class MainContainerLauncher{
+    public static void main(String[] args) {
+        Runtime rt = Runtime.instance();
+
+        Profile profile = new ProfileImpl();
+        profile.setParameter(Profile.MAIN_HOST, "localhost");
+        profile.setParameter(Profile.GUI, "true");
+        profile.setParameter(Profile.PLATFORM_ID, "RecipePlatform");
+        profile.setParameter(Profile.LOCAL_PORT, "2099");
+
+
+        AgentContainer mainContainer = rt.createMainContainer(profile);
+
+        try {
+            AgentController uiAgent = mainContainer.createNewAgent(
+                    "UIAgent",
+                    "org.example.agents.UIAgent",
+                    null
+            );
+
+            AgentController finderAgent = mainContainer.createNewAgent(
+                    "RecipeFinderAgent",
+                    "org.example.agents.RecipeFinderAgent",
+                    null
+            );
+
+            AgentController prefAgent = mainContainer.createNewAgent(
+                    "PreferenceAgent",
+                    "org.example.agents.PreferenceAgent",
+                    null
+            );
+
+            uiAgent.start();
+            finderAgent.start();
+            prefAgent.start();
+
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
+        }
+    }
 }
