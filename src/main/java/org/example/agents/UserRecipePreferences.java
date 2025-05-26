3b07f890540e64fd6df791cbcd3265663e7fd193
Viki <viktoria.tegeler@mailbox.tu-dresden.de>
2025-05-26 16:58:39 +0200

    acquisitionAgent esta enviando el json correctamente

---
+++ src/main/java/org/example/agents/UserRecipePreferences.java
@@ -0,0 +1,15 @@
+package org.example.agents;
+import java.util.List;
+
+    public class UserRecipePreferences {
+        public List<String> ingredients;
+        public List<String> allergic_information;
+        public int number_of_recipes;
+        public int max_calories;
+        public double min_rating;
+        public int max_total_time;
+        public boolean vegan;
+        public boolean vegetarian;
+
+        // Optional: Getter/Setter)
+    }
